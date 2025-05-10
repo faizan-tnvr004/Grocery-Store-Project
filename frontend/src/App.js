080e7db6af2f72ab242e1755e5e6c8eaa6473d2a
hussain-farhan <mhusssain43@gmail.com>
2025-05-10 14:43:36 +0500

    WIP: my updates to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ProductPage from './components/ProductPage';
-import CategoryPage from './components/CategoryPage'; // Assuming you have a file that exports categories
+import CategoryPage from './components/CategoryPage';
+import CartPage from './components/CartPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Fixed component name */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </Router>
   );
