a96570b5c9bde577435e51ff3cd9c04eaa37c3f6
Faizan Tanveer <160318607+faizan-tnvr004@users.noreply.github.com>
2025-05-14 00:26:36 +0500

    Delete submission directory
    accidentally added

--- submission/backend/routes/remove_from_cart.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { RemoveFromCart, clearCart } = require('../controllers/removeFromCartController');
-
-router.delete('/remove', RemoveFromCart);
-router.delete('/clear', clearCart);
-
-
-module.exports = router;
